6206bd25a8cb2d5dccbfd70de65b5a16b818736c
Davide Bianco <davidebianco1@icloud.com>
2025-08-06 12:39:02 +0200

    adjust correct profile in test

--- src/test/java/com/biancodavide3/budgeting/security/SupabaseAuthenticationIT.java
+++ src/test/java/com/biancodavide3/budgeting/security/SupabaseAuthenticationIT.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ActiveProfiles("prod")
+@ActiveProfiles("dev")
 @Disabled
 @AutoConfigureMockMvc
 class SupabaseAuthenticationIT {
