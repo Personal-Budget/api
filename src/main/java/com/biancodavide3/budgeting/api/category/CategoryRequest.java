f1290f642ad4a72ba064b69837956a55bc9072ef
Davide Bianco <davidebianco1@icloud.com>
2025-08-11 12:30:21 +0200

    add missing messages in CategoryRequest

--- src/main/java/com/biancodavide3/budgeting/api/category/CategoryRequest.java
+++ src/main/java/com/biancodavide3/budgeting/api/category/CategoryRequest.java
@@ -15,9 +15,9 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Builder
 public class CategoryRequest {
-    @NotBlank
+    @NotBlank(message = "name cannot be blank")
     private String name;
-    @NotNull
+    @NotNull(message = "goal cannot be null")
     @Digits(integer = 10, fraction = 2, message = "goal must be a valid decimal number with up to 10 digits and 2 decimal places")
     private BigDecimal goal;
 }
